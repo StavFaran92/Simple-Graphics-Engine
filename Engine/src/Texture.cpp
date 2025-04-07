b78363a5fe7020d0a27b0e3cd336a945cce0c9e0
stav faran <41070216+StavFaran92@users.noreply.github.com>
2025-04-07 22:41:24 +0300

    this fixes texture write but break cubemap

--- Engine/src/Texture.cpp
+++ Engine/src/Texture.cpp
@@ -254,11 +254,11 @@ Texture::TextureData Texture::extractTextureDataFromFile(const std::string& file
 	textureData.params = {
 		{ GL_TEXTURE_WRAP_S, GL_REPEAT},
 		{ GL_TEXTURE_WRAP_T, GL_REPEAT},
-		{ GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR},
-		{ GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR},
+		{ GL_TEXTURE_MIN_FILTER, GL_NEAREST},
+		{ GL_TEXTURE_MAG_FILTER, GL_NEAREST},
 	};
 
-	textureData.genMipMap = true; // todo fix we dont always want to generate mipmaps for loaded textures
+	textureData.genMipMap = false; // todo fix we dont always want to generate mipmaps for loaded textures
 
 	return textureData;
 }
