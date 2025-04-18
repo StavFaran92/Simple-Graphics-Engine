a9e94695a3704a6ed4717475600c9c792fbb6538
stav faran <41070216+StavFaran92@users.noreply.github.com>
2025-04-18 19:22:57 +0300

    switched to a different fbm to fix circle artifact

--- ShaderToy/Resources/Content/Shaders/VolumetricClouds_v2.glsl
+++ ShaderToy/Resources/Content/Shaders/VolumetricClouds_v2.glsl
@@ -5,33 +5,70 @@ const float MARCH_SIZE = 0.08;
 
 uniform sampler2D uNoise;
 
-float noise(vec3 x ) {
-  vec3 p = floor(x);
-  vec3 f = fract(x);
-  f = f*f*(3.0-2.0*f);
+// float noise(vec3 x ) {
+//   vec3 p = floor(x);
+//   vec3 f = fract(x);
+//   f = f*f*(3.0-2.0*f);
 
-  vec2 nuv = (p.xy+vec2(37.0,239.0)*p.z) + f.xy;
-  vec2 tex = textureLod(uNoise,(nuv+0.5)/256.0, 0.0).yx;
+//   vec2 nuv = (p.xy+vec2(37.0,239.0)*p.z) + f.xy;
+//   vec2 tex = textureLod(uNoise,(nuv+0.5)/256.0, 0.0).yx;
 
-  return mix( tex.x, tex.y, f.z ) * 2.0 - 1.0;
+//   return mix( tex.x, tex.y, f.z ) * 2.0 - 1.0;
+// }
+
+// float fbm(vec3 p) {
+//   vec3 q = p + iTime * 0.5 * vec3(1.0, -0.2, -1.0);
+//   float g = noise(q);
+
+//   float f = 0.0;
+//   float scale = 0.5;
+//   float factor = 2.02;
+
+//   for (int i = 0; i < 6; i++) {
+//       f += scale * noise(q);
+//       q *= factor;
+//       factor += 0.21;
+//       scale *= 0.5;
+//   }
+
+//   return f;
+// }
+
+// noise
+// Volume raycasting by XT95
+// https://www.shadertoy.com/view/lss3zr
+mat3 m = mat3( 0.00,  0.80,  0.60,
+              -0.80,  0.36, -0.48,
+              -0.60, -0.48,  0.64 );
+float hash( float n )
+{
+    return fract(sin(n)*43758.5453);
 }
 
-float fbm(vec3 p) {
-  vec3 q = p + iTime * 0.5 * vec3(1.0, -0.2, -1.0);
-  float g = noise(q);
+float noise( in vec3 x )
+{
+    vec3 p = floor(x);
+    vec3 f = fract(x);
 
-  float f = 0.0;
-  float scale = 0.5;
-  float factor = 2.02;
+    f = f*f*(3.0-2.0*f);
 
-  for (int i = 0; i < 6; i++) {
-      f += scale * noise(q);
-      q *= factor;
-      factor += 0.21;
-      scale *= 0.5;
-  }
+    float n = p.x + p.y*57.0 + 113.0*p.z;
 
-  return f;
+    float res = mix(mix(mix( hash(n+  0.0), hash(n+  1.0),f.x),
+                        mix( hash(n+ 57.0), hash(n+ 58.0),f.x),f.y),
+                    mix(mix( hash(n+113.0), hash(n+114.0),f.x),
+                        mix( hash(n+170.0), hash(n+171.0),f.x),f.y),f.z);
+    return res;
+}
+
+float fbm( vec3 p )
+{
+    float f;
+    f  = 0.5000*noise( p ); p = m*p*2.02;
+    f += 0.2500*noise( p ); p = m*p*2.03;
+    f += 0.12500*noise( p ); p = m*p*2.01;
+    f += 0.06250*noise( p );
+    return f;
 }
 
 float sdSphere(vec3 p, float radius) 
@@ -43,7 +80,8 @@ float sceneSDF(vec3 pos)
 {
     float distance = sdSphere(pos, 1);
 
-    float f = fbm(pos);
+    vec3 q = pos - vec3(1.0,0.2,1.0)*iTime;
+    float f = fbm(q);
 
     return -distance + f;
 }
