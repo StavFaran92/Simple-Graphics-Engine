77cccd3bfcf18a7a3596d32eb4798b53d29df474
stav faran <41070216+StavFaran92@users.noreply.github.com>
2025-07-28 22:36:13 +0300

    working on patch adjusrtments

--- Engine/src/component/FoliageComponent.cpp
+++ Engine/src/component/FoliageComponent.cpp
@@ -7,15 +7,13 @@
 
 void FoliageComponent::build()
 {
-	
-
 	m_patchCount = glm::vec2(ceil(width / patchWidth), ceil(height / patchHeight));
 
 	m_patches.clear();
 	m_patches.reserve(m_patchCount.x * m_patchCount.y);
-	for (int i = 0; i < m_patchCount.x; i++)
+	for (int i = 0; i < m_patchCount.y; i++) // Rows
 	{
-		for (int j = 0; j < m_patchCount.y; j++)
+		for (int j = 0; j < m_patchCount.x; j++) // Cols
 		{
 			FoliagePatch patch;
 			patch.pos = glm::vec3(i * patchWidth, 0, j * patchHeight);
@@ -28,12 +26,13 @@ void FoliageComponent::build()
 	}
 
 	m_foliageSpreadMap->bind();
-	std::vector<GLubyte> pixels(m_foliageSpreadMap->getWidth() * m_foliageSpreadMap->getHeight() * m_foliageSpreadMap->getBitDepth());
-	glGetTexImage(GL_TEXTURE_2D, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels.data());
+	std::vector<GLubyte> pixels(m_foliageSpreadMap->getWidth() * m_foliageSpreadMap->getHeight());
+	glGetTexImage(GL_TEXTURE_2D, 0, GL_RED, GL_UNSIGNED_BYTE, pixels.data());
 
 	for (auto& p : m_patches)
 	{
-		int index = (p.idy * width + p.idx) * 3;
+		int index = (p.idy * width + p.idx);
+		//index = index % (int)m_patchCount.x * (int)m_patchCount.y;
 		GLubyte r = pixels[index];
 		p.density = r / 255.f;
 	}
