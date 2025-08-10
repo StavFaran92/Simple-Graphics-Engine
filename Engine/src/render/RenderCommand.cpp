d77e60043e19aa7414d8c5c3fb25fe643f00dcd9
stav faran <41070216+StavFaran92@users.noreply.github.com>
2025-08-10 22:07:17 +0300

    Use window size when blitting to default framebuffer

--- Engine/src/render/RenderCommand.cpp
+++ Engine/src/render/RenderCommand.cpp
@@ -1,6 +1,7 @@
 #include "render/RenderCommand.h"
 #include "core/Engine.h"
 #include "core/System.h"
+#include "core/Window.h"
 #include "render/Graphics.h"
 #include "render/RenderView.h"
 #include "render/VertexArrayObject.h"
@@ -56,7 +57,7 @@ void RenderCommand::setViewport(int x, int y, int w, int h)
 
 void RenderCommand::copyFrameBufferData(unsigned int src, unsigned int dst, int bufferBit)
 {
-	auto graphics = Engine::get()->getSubSystem<Graphics>();
+        auto graphics = Engine::get()->getSubSystem<Graphics>();
 
 	// Bind G-Buffer as src
 	glBindFramebuffer(GL_READ_FRAMEBUFFER, src);
@@ -64,19 +65,34 @@ void RenderCommand::copyFrameBufferData(unsigned int src, unsigned int dst, int 
 	// Bind default buffer as dest
 	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, dst);
 
-	auto renderView = graphics->renderView;
+        auto renderView = graphics->renderView;
 
-	assert(renderView);
+        assert(renderView);
 
-	auto& viewport = renderView->getViewport();
+        auto& viewport = renderView->getViewport();
 
-	// Copy src to dest
-	glBlitFramebuffer(0, 0, viewport.w, viewport.h,
-		0, 0, viewport.w, viewport.h,
-		bufferBit, GL_NEAREST);
+        int destW = viewport.w;
+        int destH = viewport.h;
+
+        // If copying to the default framebuffer, use the window dimensions as
+        // the destination size to ensure the final image covers the window.
+        if (dst == 0)
+        {
+                auto window = Engine::get()->getWindow();
+                if (window)
+                {
+                        destW = window->getWidth();
+                        destH = window->getHeight();
+                }
+        }
+
+        // Copy src to dest
+        glBlitFramebuffer(0, 0, viewport.w, viewport.h,
+                0, 0, destW, destH,
+                bufferBit, GL_NEAREST);
 
 
-	glBindFramebuffer(GL_FRAMEBUFFER, dst);
+        glBindFramebuffer(GL_FRAMEBUFFER, dst);
 }
 
 //void RenderCommand::drawIndexed(const VertexArrayObject* vao)
