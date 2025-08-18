655cc9520d2391f44f6f5f70da0110a72e2368a7
stav faran <41070216+StavFaran92@users.noreply.github.com>
2025-08-18 23:41:57 +0300

    Add serialization for FoliageComponent

--- Engine/src/component/FoliageComponent.h
+++ Engine/src/component/FoliageComponent.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include "core/Core.h"
-
 #include "component/Component.h"
+#include "serialize/CerealHelpers.h"
+#include <vector>
 
 struct FoliagePatch
 {
@@ -17,13 +18,26 @@ struct FoliagePatch
 
 struct EngineAPI FoliageComponent : public Component
 {
-	FoliageComponent() = default;
+        FoliageComponent() = default;
 
-	void build();
+        void build();
 
-	glm::vec2 getPatchCount() const;
+        glm::vec2 getPatchCount() const;
 
-	const std::vector<FoliagePatch>& getPatches() const;
+        const std::vector<FoliagePatch>& getPatches() const;
+
+        template <class Archive>
+        void serialize(Archive& archive) {
+                SERIALIZED_MEMBER(m_foliageSpreadMap);
+                SERIALIZED_MEMBER(globalDensity);
+                SERIALIZED_MEMBER(colorA);
+                SERIALIZED_MEMBER(colorB);
+                SERIALIZED_MEMBER(patchWidth);
+                SERIALIZED_MEMBER(patchHeight);
+                SERIALIZED_MEMBER(width);
+                SERIALIZED_MEMBER(height);
+                SERIALIZED_MEMBER(terrainRef);
+        }
 
 	Resource<Texture> m_foliageSpreadMap;
 	float globalDensity = 1.f;
