745354b5104e715941b130e94f5d707eaffcf366
stav faran <41070216+StavFaran92@users.noreply.github.com>
2025-08-23 23:42:56 +0300

    remove image comp from comp file

--- Engine/src/component/Component.h
+++ Engine/src/component/Component.h
@@ -106,29 +106,6 @@ private:
 
 };
 
-
-struct EngineAPI ImageComponent : public Component
-{
-	ImageComponent() = default;
-
-	ImageComponent(Resource<Texture> image) : image(image) {}
-
-	template <class Archive>
-	void serialize(Archive& archive) {
-		SERIALIZED_MEMBER(image);
-		SERIALIZED_MEMBER(size);
-		SERIALIZED_MEMBER(position);
-		SERIALIZED_MEMBER(rotate);
-	}
-
-	glm::vec2 size;
-	glm::vec2 position;
-	float rotate = 0;
-
-    Resource<Texture> image;
-    static void attachToEntity(std::shared_ptr<Component>, Entity, Scene&);
-};
-
 struct TestComp : public Component
 {
 	TestComp() = default;
