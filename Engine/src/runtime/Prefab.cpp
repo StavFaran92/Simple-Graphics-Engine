aa813f786f5b62b4d37db4b8d8c8414e2c7bbea3
ADI-PC\stavf <stav12212@gmail.com>
2025-08-25 19:40:10 +0300

    started working on prefav impl

--- Engine/src/runtime/Prefab.cpp
+++ Engine/src/runtime/Prefab.cpp
@@ -1,17 +1,32 @@
 #include "Prefab.h"
 
+#include <filesystem>
+
 static AssetFnRegister<AssetType::PREFAB> assetRegister(Prefab::load);
 
 Resource<Prefab> Prefab::import(const std::string& fileLocation, const PrefabImportSettings& settings)
 {
+	if (fileLocation.empty() || !std::filesystem::exists(fileLocation))
+	{
+		logWarning("Invalid file location specified.");
+		return Resource<Prefab>::empty;
+	}
+
+	std::filesystem::path file(fileLocation);
+	if (file.extension() != ".asset")
+	{
+		logWarning("Invalid file type specified, prefab should have .asset extension.");
+		return Resource<Prefab>::empty;
+	}
+
 	AssetInfo aInfo;
 	aInfo.uuid = "test";
-	aInfo.origFilePath = "C:/Users/Stav/Downloads/blueprint_8974271.png";
+	aInfo.origFilePath = fileLocation;
 	aInfo.aType = AssetType::PREFAB;
-	aInfo.name = "test";
+	aInfo.name = file.filename().stem().string();
 	Engine::get()->getSubSystem<Assets>()->importAsset(aInfo);
 
-	return Resource<Prefab>::empty;
+	return load(aInfo);
 }
 
 Resource<Prefab> Prefab::load(AssetInfo aInfo)
