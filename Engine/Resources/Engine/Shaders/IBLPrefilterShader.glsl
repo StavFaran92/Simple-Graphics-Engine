9bbb369671555bc53f091ff10d8ff3466b8ee006
stav faran <41070216+StavFaran92@users.noreply.github.com>
2025-11-01 07:37:53 +0200

    added mip level sampling for prefilter map to combat bright sots artifact

--- Engine/Resources/Engine/Shaders/IBLPrefilterShader.glsl
+++ Engine/Resources/Engine/Shaders/IBLPrefilterShader.glsl
@@ -82,6 +82,20 @@ vec3 ImportanceSampleGGX(vec2 Xi, vec3 N, float roughness)
     return normalize(sampleVector);
 }
 
+float DistributionGGX(vec3 N, vec3 H, float roughness)
+{
+    float a = roughness*roughness;
+    float a2 = a*a;
+    float NdotH = max(dot(N, H), 0.0);
+    float NdotH2 = NdotH*NdotH;
+
+    float nom   = a2;
+    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+    denom = PI * denom * denom;
+
+    return nom / denom;
+}
+
 void main()
 {
     vec3 N = normalize(localPos);
@@ -100,17 +114,41 @@ void main()
         vec3 L  = normalize(2.0 * dot(V, H) * H - V); // I dont understand why use dot here
 
         float NdotL = max(0.0, dot(N, L)); 
-
-        vec3 sample = texture(environmentMap, L).rgb;
-
-        if(length(sample) > 10000)
+        if(NdotL > 0.0)
         {
-            continue;
+            // sample from the environment's mip level based on roughness/pdf
+            float D   = DistributionGGX(N, H, roughness);
+            float NdotH = max(dot(N, H), 0.0);
+            float HdotV = max(dot(H, V), 0.0);
+            float pdf = D * NdotH / (4.0 * HdotV) + 0.0001; 
+
+            float resolution = 512.0; // resolution of source cubemap (per face)
+            float saTexel  = 4.0 * PI / (6.0 * resolution * resolution);
+            float saSample = 1.0 / (float(numOfSamples) * pdf + 0.0001);
+
+            float mipLevel = roughness == 0.0 ? 0.0 : 0.5 * log2(saSample / saTexel); 
+
+            vec3 sample = textureLod(environmentMap, L, mipLevel).rgb;
+
+            if(length(sample) > 10000)
+            {
+                continue;
+            }
+
+            prefilteredColor += sample * NdotL;
+            totalWeight += NdotL;
         }
 
-        prefilteredColor += sample * NdotL;
+        // vec3 sample = texture(environmentMap, L).rgb;
 
-        totalWeight += NdotL;
+        // if(length(sample) > 10000)
+        // {
+        //     continue;
+        // }
+
+        // prefilteredColor += sample * NdotL;
+
+        // totalWeight += NdotL;
     }
 
     prefilteredColor /= totalWeight;
