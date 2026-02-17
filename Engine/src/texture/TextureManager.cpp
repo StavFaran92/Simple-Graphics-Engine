85b56d298cf830513801ec87272225eac9cb84ea
stav faran <41070216+StavFaran92@users.noreply.github.com>
2026-02-17 20:22:41 +0200

    implemented resource save for texture

--- Engine/src/texture/TextureManager.cpp
+++ Engine/src/texture/TextureManager.cpp
@@ -22,7 +22,35 @@ bool TextureTypeManager::importAsset(const std::string& src, const ScopedPath& d
 
 bool TextureTypeManager::saveResource(const ResourceCreateDescriptor& desc, const ScopedPath& dst)
 {
-	return false;
+	auto textureDesc = dynamic_cast<const TextureCreateDescriptor*>(&desc);
+	if (!textureDesc)
+	{
+		logError("Invalid Descriptor specified.");
+		return false;
+	}
+
+	const TextureData& data = textureDesc->textureData;
+
+	if (data.type == TextureType::FLOAT)
+	{
+		EXRLoader::saveSingleChannelEXR(dst.absolute().string(), 
+			data.width, 
+			data.height, 
+			(const float*)data.data);
+	}
+	else
+	{
+		STBIHelper::writeToPNG(dst.absolute().string(), 
+			data.width, 
+			data.height, 
+			data.channels, 
+			data.data, 
+			data.width * data.channels);
+
+
+	}
+
+	return true;
 }
 
 ResourceLoadDescriptor* TextureTypeManager::makeResourceLoadDescriptor()
