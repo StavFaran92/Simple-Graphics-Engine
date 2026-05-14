84dc56c029ea76580e6080f87945b7c2580150e7
stav faran <41070216+StavFaran92@users.noreply.github.com>
2026-05-14 10:53:02 +0300

    make enemy rotate player

--- samples/game/Content/Mutant/enemyController.lua
+++ samples/game/Content/Mutant/enemyController.lua
@@ -5,7 +5,10 @@ Script = {}
 function Script:create(entity)
     self.player = getActiveScene():getEntityByName("player")
     self.physics = entity.Physics
+    self.model = entity:getChildByName("model")
+    self.modelTransform = entity:getChildByName("model").Transform
     self.speed = 10.0
+    self.animator = self.model.Animator
 end
 
 function Script:update(entity, dt)
@@ -16,6 +19,21 @@ function Script:update(entity, dt)
     local movement = vec3.new(dir.x, 0, dir.z) * self.speed
 
     self.physics:setForce(movement)
+
+    if math.abs(dir.x) > 0.0 or math.abs(dir.y) > 0.0 then
+        local angle = -math.atan(dir.z, dir.x)
+        self.modelTransform:setLocalRotation(angle + math.pi / 2, vec3.new(0, 1, 0))
+
+        local animState = "Run"
+        if self.animator:getCurrentAnimationName() ~= animState then
+            self.animator:playAnimation(animState)
+        end
+    else
+        local animState = "Idle"
+        if self.animator:getCurrentAnimationName() ~= animState then
+            self.animator:playAnimation(animState)
+        end
+    end
 end
 
 function Script:destroy(entity)
