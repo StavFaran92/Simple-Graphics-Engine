43d8a971fe2db22253c4fd8055686b4b70d31828
stav faran <41070216+StavFaran92@users.noreply.github.com>
2026-05-31 00:30:02 +0300

    added lphysics layer name edit

--- EditorApp/src/Dialogs/ProjectSettingsDialog.cpp
+++ EditorApp/src/Dialogs/ProjectSettingsDialog.cpp
@@ -4,6 +4,7 @@
 #include <imgui.h>
 
 #include "render/Graphics.h"
+#include "core/ProjectSettings.h"
 
 ProjectSettingsDialog::ProjectSettingsDialog()
 	: DialogBase("ProjectSettingsDialog")
@@ -16,12 +17,49 @@ void ProjectSettingsDialog::appearContent()
 
 void ProjectSettingsDialog::drawContent()
 {
-	auto graphics = Engine::get()->getSubSystem<Graphics>();
-	ImGui::Checkbox("SSAO", &graphics->useSSAO);
+	if (!ImGui::BeginTabBar("ProjectSettingsTabs"))
+		return;
+
+	if (ImGui::BeginTabItem("Graphics"))
+	{
+		auto graphics = Engine::get()->getSubSystem<Graphics>();
+		ImGui::Checkbox("SSAO", &graphics->useSSAO);
+		ImGui::EndTabItem();
+	}
+
+	if (ImGui::BeginTabItem("Physics"))
+	{
+		if (ImGui::CollapsingHeader("Layer Masks")) 
+		{
+			auto& ps = ProjectSettings::get();
+			ImGui::BeginChild("LayersList", ImVec2(300, 400), false);
+			for (int i = 0; i < 32; i++)
+			{
+				char buf[128];
+				std::string name = ps.getLayerName(i);
+				strncpy_s(buf, name.c_str(), sizeof(buf) - 1);
+				buf[sizeof(buf) - 1] = '\0';
+
+				ImGui::PushID(i);
+				ImGui::Text("%2d", i);
+				ImGui::SameLine();
+				ImGui::SetNextItemWidth(-1);
+				if (ImGui::InputText("##layer", buf, sizeof(buf)))
+					ps.setLayerName(i, buf);
+				ImGui::PopID();
+			}
+			ImGui::EndChild();
+			
+		}
+		ImGui::EndTabItem();
+	}
+
+	ImGui::EndTabBar();
 }
 
 bool ProjectSettingsDialog::acceptContent()
 {
+	ProjectSettings::get().save();
 	return true;
 }
 
