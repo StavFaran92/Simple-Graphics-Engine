8e46e3f37de700ddaf6ca4592a91b9eeea15b4f2
stav faran <41070216+StavFaran92@users.noreply.github.com>
2026-09-21 14:36:02 +0300

    added semi sebug config

--- samples/game/Content/gen_enemies.lua
+++ samples/game/Content/gen_enemies.lua
@@ -8,11 +8,13 @@ function Script:create(entity)
     local gridSize = 50
     local spacing = 1
 
-    for i = 0, gridSize - 1 do
-        for j = 0, gridSize - 1 do
-            prefab:instansiate(vec3.new(i * spacing, 0, j * spacing))
-        end
-    end
+    -- for i = 0, gridSize - 1 do
+    --     for j = 0, gridSize - 1 do
+    --         prefab:instansiate(vec3.new(i * spacing, 0, j * spacing))
+    --     end
+    -- end
+
+    prefab:instansiate(vec3.new(0, 0, 0))
     -- initialization logic
 end
 
